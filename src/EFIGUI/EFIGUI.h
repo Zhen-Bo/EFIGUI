2e0f552268585b6439f164b28c744771c297dd6b
KevinSun <paver.mails+github@gmail.com>
2025-12-29 13:56:19 +0800

    chore: bump version to 0.3.2

--- src/EFIGUI/EFIGUI.h
+++ src/EFIGUI/EFIGUI.h
@@ -9,8 +9,8 @@
 // Version
 #define EFIGUI_VERSION_MAJOR 0
 #define EFIGUI_VERSION_MINOR 3
-#define EFIGUI_VERSION_PATCH 1
-#define EFIGUI_VERSION_STRING "0.3.1"
+#define EFIGUI_VERSION_PATCH 2
+#define EFIGUI_VERSION_STRING "0.3.2"
 
 // Core headers
 #include "Theme.h"
